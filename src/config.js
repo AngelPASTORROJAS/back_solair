d58d3c79f00a3ce395ca7ce8fecec3f03f615324
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-08 10:27:19 +0200

    feat: url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 const PORT = process.env.PORT || 3000;
-const DB_URL = process.env.DB_URL || '';
+const DB_URL = process.env.DB_URL || 'mysql://root:1QUzcmqbVUzyyrImekDb@containers-us-west-191.railway.app:7435/railway';
 const USER_TABLE = process.env.USER_TABLE || "Utilisateur";
 const DESTINATION_TABLE = process.env.DESTINATION_TABLE || "Destination";
 const HASH_ROUNDS = process.env.HASH_ROUNDS || 10;
