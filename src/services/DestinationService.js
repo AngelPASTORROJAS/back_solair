f0c1ab2e3aa7a849c1728e81a9e0d577396c4d27
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-08 02:12:08 +0200

    fix getRamdomDestination renvoie 2 destination aléatoire

--- src/services/DestinationService.js
+++ src/services/DestinationService.js
@@ -53,7 +53,7 @@ class DestinationService {
     return result.affectedRows > 0;  }
   
     static async getRandomDestination() {
-      const query = `SELECT id, nom, urlimage, description FROM ${DESTINATION_TABLE} order by RAND() LIMIT 1`;
+      const query = `SELECT id, nom, urlimage, description FROM ${DESTINATION_TABLE} order by RAND() LIMIT 2`;
       const [rows] = await executeQuery(query);
       return rows;
     }
