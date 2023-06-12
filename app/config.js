d346d91843f64bcb0a75933b885a635eecf4c62a
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-12 16:41:56 +0200

    feat: renommage du fichier src en app

--- app/config.js
+++ app/config.js
@@ -6,5 +6,6 @@ const DB_URL = process.env.DB_URL || '';
 const USER_TABLE = process.env.USER_TABLE || "Utilisateur";
 const DESTINATION_TABLE = process.env.DESTINATION_TABLE || "Destination";
 const HASH_ROUNDS = process.env.HASH_ROUNDS || 10;
+const JWT_EXPIRATION = process.env.JWT_EXPIRATION || "1h";
 
-export { PORT, DB_URL, USER_TABLE, HASH_ROUNDS, DESTINATION_TABLE }
+export { PORT, DB_URL, USER_TABLE, HASH_ROUNDS, DESTINATION_TABLE, JWT_EXPIRATION }
