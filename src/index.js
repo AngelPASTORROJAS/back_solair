8d6984b8a103a4d660d7ca737c559877aeeb635e
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-14 11:55:28 +0200

    chore: ajout du nom du serveur en cours l'execution

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import app from "./app.js";
 
 if(NODE_ENV){
     app.listen(PORT);
-    console.log(`Server on port http://localhost:${PORT}`);
+    console.log(`Le serveur de ${NODE_ENV} est en cours d'exécution, sur le port ${PORT}.\nhttp://localhost:${PORT}`);
 }else{
-    console.log('Choississez un environement, \nvotre environement en vide');
+    console.log('Choississez un environement, votre environement en vide');
 }
