300346404543d27d282d84dd667cc30be8ba3f32
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-14 11:28:29 +0200

    feat: ajout de plusieur environnement .env

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,13 @@
 import { config } from "dotenv";
 config();
 
+const NODE_ENV = process.env.NODE_ENV;
+const environnement='prod'
+
+if(process.env.NODE_ENV==environnement){
+    config({path:`${environnement}.env`, encoding: 'latin1', override: true})
+}
+
 const PORT = process.env.PORT || 3000;
 const DB_CONFIG = Object.freeze({
     host: process.env.DB_HOST || "exempleHost",
@@ -18,4 +25,4 @@ const HASH_ROUNDS = process.env.HASH_ROUNDS || 10;
 const JWT_EXPIRATION = process.env.JWT_EXPIRATION || "1h";
 
 
-export { PORT, DB_CONFIG, USER_TABLE, HASH_ROUNDS, DESTINATION_TABLE, JWT_EXPIRATION }
+export { NODE_ENV, PORT, DB_CONFIG, USER_TABLE, HASH_ROUNDS, DESTINATION_TABLE, JWT_EXPIRATION }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import { NODE_ENV, PORT } from "./config/config.js";
 import app from "./app.js";
-import { PORT } from "./config/config.js";
 
-app.listen(PORT);
-console.log(`Server on port http://localhost:${PORT}`);
+if(NODE_ENV){
+    app.listen(PORT);
+    console.log(`Server on port http://localhost:${PORT}`);
+}else{
+    console.log('Choississez un environement, \nvotre environement en vide');
+}
