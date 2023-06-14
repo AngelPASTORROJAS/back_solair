d64856eed254b9e0d900135a6b62026d7f2750ae
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-14 11:10:53 +0200

    feat: mise en commentaire sql pour l'ajout d'une colonne, son renommage, et sa suppression

--- database/ALTER.sql
+++ database/ALTER.sql
@@ -1,2 +1,5 @@
 ALTER TABLE Destination MODIFY description TEXT;
 ALTER TABLE Destination MODIFY urlimage TEXT;
+-- ALTER TABLE Utilisateur ADD COLUMN roles VARCHAR(150);
+-- ALTER TABLE Utilisateur CHANGE roles role VARCHAR(150);
+-- ALTER TABLE Utilisateur DROP COLUMN role;
