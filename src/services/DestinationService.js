cbb5b207101786fbc69f54c25ee9f519c2745cdb
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-06-15 18:45:30 +0200

    style: rennommage de variable

--- src/services/DestinationService.js
+++ src/services/DestinationService.js
@@ -2,38 +2,39 @@ import db from "../config/db.js";
 
 class DestinationService {
   static async getAllDestination() {
-    const query = `SELECT id, nom, urlimage, description FROM ${db.destination}`;
-    return db.query(query);
+    const sql = `SELECT id, nom, urlimage, description FROM ${db.destination}`;
+    return db.query(sql);
   }
 
   static async getDestinationById(id) {
-    const query = `SELECT id, nom, urlimage, description FROM ${db.destination} WHERE id = ? LIMIT 1`;
-    return db.query(query, [id]);
+    const sql = `SELECT id, nom, urlimage, description FROM ${db.destination} WHERE id = ? LIMIT 1`;
+    return db.query(sql, [id]);
   }
 
   static async createDestination(nom, urlimage, description) {
-    const query = `INSERT INTO ${db.destination} (nom, urlimage, description) VALUES (?, ?, ?)`;
-    const result = db.query(query, [nom, urlimage, description]);
+    const sql = `INSERT INTO ${db.destination} (nom, urlimage, description) VALUES (?, ?, ?)`;
+    const values = [nom, urlimage, description];
+    const result = db.query(sql, values);
     return result.affectedRows > 0;
   }
 
   static async patchDestinationById(id, nom, urlimage, description) {
-    const params = [];
+    const values = [];
     const columns = [];
 
     if (nom !== undefined) {
       columns.push("nom = ?");
-      params.push(nom);
+      values.push(nom);
     }
 
     if (urlimage !== undefined) {
       columns.push("urlimage = ?");
-      params.push(urlimage);
+      values.push(urlimage);
     }
 
     if (description !== undefined) {
       columns.push("description = ?");
-      params.push(description);
+      values.push(description);
     }
 
     if (columns.length === 0) {
@@ -42,15 +43,15 @@ class DestinationService {
 
     const setClause = columns.join(", ");
 
-    params.push(id);
-    const query = `UPDATE ${db.destination} SET ${setClause} WHERE id = ?`;
-    const result = db.query(query, params);
+    values.push(id);
+    const sql = `UPDATE ${db.destination} SET ${setClause} WHERE id = ?`;
+    const result = db.query(sql, values);
     return result.affectedRows > 0;
   }
 
   static async getRandomDestination() {
-    const query = `SELECT id, nom, urlimage, description, ville FROM ${db.destination} order by RAND() LIMIT 1`;
-    return db.query(query);
+    const sql = `SELECT id, nom, urlimage, description, ville FROM ${db.destination} order by RAND() LIMIT 1`;
+    return db.query(sql);
   }
 }
 
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,28 +3,25 @@ import { checkPasswordMatch, encryptPassword } from "../utils/password.js";
 
 class UserService {
   static async getAllUsers() {
-    const query = `SELECT id, pseudo, mail FROM ${db.utilisateur}`;
-    return db.query(query);
+    const sql = `SELECT id, pseudo, mail FROM ${db.utilisateur}`;
+    return db.query(sql);
   }
 
   static async getUserById(id) {
-    const query = `SELECT id, pseudo, mail FROM ${db.utilisateur} WHERE id = ?`;
-    return db.query(query, [id]);
+    const sql = `SELECT id, pseudo, mail FROM ${db.utilisateur} WHERE id = ?`;
+    return db.query(sql, [id]);
   }
 
   static async createUser(pseudo, mail, motdepasse) {
-    const query = `INSERT INTO ${db.utilisateur} (pseudo, mail, motdepasse) VALUES (?, ?, ?)`;
-    const result = db.query(query, [
-      pseudo,
-      mail,
-      await encryptPassword(motdepasse),
-    ]);
+    const sql = `INSERT INTO ${db.utilisateur} (pseudo, mail, motdepasse) VALUES (?, ?, ?)`;
+    const values = [ pseudo,mail, await encryptPassword(motdepasse) ];
+    const result = db.query(sql, values);
     return result.affectedRows > 0;
   }
 
   static async authenticateUser(mail, motdepasse) {
-    const query = `SELECT motdepasse FROM ${db.utilisateur} WHERE mail = ? LIMIT 1`;
-    const rows = db.query(query, [mail]);
+    const sql = `SELECT motdepasse FROM ${db.utilisateur} WHERE mail = ? LIMIT 1`;
+    const rows = db.query(sql, [mail]);
     return (
       rows.length > 0 &&
       (await checkPasswordMatch(motdepasse, rows[0].motdepasse))
@@ -32,30 +29,30 @@ class UserService {
   }
 
   static async patchUserById(id, pseudo, mail, motdepasse) {
-    const params = [];
+    const values = [];
     const clause = [];
 
     if (pseudo !== undefined) {
       clause.push("pseudo = ?");
-      params.push(pseudo);
+      values.push(pseudo);
     }
 
     if (mail !== undefined) {
       clause.push("mail = ?");
-      params.push(mail);
+      values.push(mail);
     }
 
     if (motdepasse !== undefined) {
       clause.push("motdepasse = ?");
-      params.push(await encryptPassword(motdepasse));
+      values.push(await encryptPassword(motdepasse));
     }
-    if (params.length == 0) {
+    if (values.length == 0) {
       return true; // Aucune colonne à mettre à jour
     }
     const setClause = clause.join(", ");
-    params.push(id);
-    const query = `UPDATE ${db.utilisateur} SET ${setClause} WHERE id = ?`;
-    const result = db.query(query, params);
+    values.push(id);
+    const sql = `UPDATE ${db.utilisateur} SET ${setClause} WHERE id = ?`;
+    const result = db.query(sql, values);
     return result.affectedRows > 0;
   }
 }
