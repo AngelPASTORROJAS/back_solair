6f256ae979716a3e09796409773b92f553695857
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-07-03 11:35:11 +0200

    freat: ajout de checkJwt comme middleware pour la route pour récuperer le utilisateurs

--- src/routes/UtilisateurRouter.js
+++ src/routes/UtilisateurRouter.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { utilisateurController } from "../controllers/UserController.js";
 import { ValidateUserInput, ValidateAuthInput } from "../middlewares/UserMiddleware.js";
 import apicache from "apicache";
+import { checkJwt } from "../middlewares/auth/Jwt.js";
 
 const UtilisateurRouter = Router();
 
 UtilisateurRouter
-  .get("", utilisateurController.getAllUsers)
+  .get("", checkJwt, apicache.middleware("5 minutes"), utilisateurController.getAllUsers)
   .get("/:id([0-9]+)", apicache.middleware("5 minutes"), utilisateurController.getUserById)
   .post("", ValidateUserInput, utilisateurController.createUser)
   .post("/authenticate", ValidateAuthInput, utilisateurController.authenticateUser)
