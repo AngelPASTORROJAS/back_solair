a64d0a9b547c3e6dbf0fc6f6a61e5ba400c68742
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-07-03 21:40:07 +0200

    feat: ajout des clause guad pour checkJwt + fix du bon fonctionnement de requireRole

--- src/middlewares/auth/Jwt.js
+++ src/middlewares/auth/Jwt.js
@@ -1,49 +1,73 @@
 import jwt from "jsonwebtoken";
 import { HttpStatus } from "../../http/httpStatus.js";
 import { SECRET_KEY, JWT_EXPIRATION } from "../../config/config.js";
+// eslint-disable-next-line no-unused-vars
+import { Role } from "../../models/Role.js";
+
 
 const getToken = (payload) => {
   return jwt.sign(payload, SECRET_KEY, {expiresIn:JWT_EXPIRATION});
 };
 
 const checkJwt = (req, res, next) => {
+  // Vérifier si req.headers.authorization est existe
+  if (!req.headers.authorization) {
+    return res
+      .status(HttpStatus.UNAUTHORIZED.code)
+      .json({ message: HttpStatus.UNAUTHORIZED.message });
+  }
   // Récupérer le token JWT dans l'en-tête Authorization de la requête
-  let token = req.headers["authorization"];
+  const token = req.headers.authorization.split(" ")[1];
 
-  // Vérifier si le token est présent dans l'en-tête
+  // Vérifie si le jeton d'authentification est manquant.
   if (!token) {
-    req.decoded = null;
-    // Renvoyer une réponse d'erreur d'authentification si le token est manquant
     return res
       .status(HttpStatus.UNAUTHORIZED.code)
       .json({ message: HttpStatus.UNAUTHORIZED.message });
   }
 
-  // Supprimer le préfixe "Bearer " du token JWT
-  token = token.replace("Bearer ", "");
-
   let jwtPayload;
   try {
-    // Décoder le token JWT sans vérifier son authenticité
+    // Décoder le token JWT sans vérifier son authenticité 
     jwtPayload = jwt.decode(token, { complete: true });
     // Stocker l'en-tête et le payload décodés dans l'objet req.decoded
     req.decoded = jwtPayload;
   } catch (error) {
-    // Renvoyer une réponse d'erreur d'authentification si le token est invalide ou expiré
+    // Le jeton d'authentification est invalide ou expiré
     return res
       .status(HttpStatus.UNAUTHORIZED.code)
       .json({ message: HttpStatus.UNAUTHORIZED.message });
   }
-  // Passer au middleware suivant si le token est valide
   next();
 };
 
 const requireRole = (role) => {
   return (req, res, next) => {
-    if (req.decoded && req.decoded.roles.includes(role)) {
+    // Vérifier si req.decoded est défini
+    if (typeof req.decoded === "undefined") {
+      return res
+        .status(HttpStatus.FORBIDDEN.code)
+        .json({ message: HttpStatus.FORBIDDEN.message});
+    }
+
+    // Vérifier si req.decoded.roles est défini
+    if (typeof req.decoded.roles === "undefined") {
+      return res
+        .status(HttpStatus.FORBIDDEN.code)
+        .json({ message: HttpStatus.FORBIDDEN.message });
+    }
+
+    /**@type {Role[]} */
+    const roles =  req.decoded.roles;
+
+    // Vérifier si l'utilisateur a le rôle requis
+    const hasRole = roles.some((r) => r.nom === role);
+
+    if (hasRole) {
+      // continuer l'exécution de la requête
       next();
     } else {
-      res
+      return res
         .status(HttpStatus.FORBIDDEN.code)
         .json({ message: HttpStatus.FORBIDDEN.message });
     }
