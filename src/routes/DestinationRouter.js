73a393b66555e197e20872c1c3bb6870e180c866
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-07-04 02:15:03 +0200

    feat: ajout de la vérification des routes destination par un token

--- src/routes/DestinationRouter.js
+++ src/routes/DestinationRouter.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { destinationController } from "../controllers/DestinationController.js";
 import apicache from "apicache";
+import { checkJwt } from "../middlewares/auth/Jwt.js";
 
 const DestinationRouter = Router();
 
 DestinationRouter
-  .get("", apicache.middleware("5 minutes"), destinationController.getAllDestination)
-  .get("/random", destinationController.getRandomDestination)
-  .get("/:id([0-9]+)", apicache.middleware("5 minutes"), destinationController.getDestinationById)
-  .post("", destinationController.createDestination)
-  .patch("/:id([0-9]+)", destinationController.patchDestinationById);
+  .get("", checkJwt, apicache.middleware("5 minutes"), destinationController.getAllDestination)
+  .get("/random", checkJwt, destinationController.getRandomDestination)
+  .get("/:id([0-9]+)", checkJwt, apicache.middleware("5 minutes"), destinationController.getDestinationById)
+  .post("",  checkJwt, destinationController.createDestination)
+  .patch("/:id([0-9]+)",  checkJwt, destinationController.patchDestinationById);
 
 export { DestinationRouter };
